aa9ce29b87928930a7fa8ed9e0fc7432cce9489e
xxyeon <jykim9335@gmail.com>
2023-11-18 15:09:49 +0900

    feat: init project settings

---
+++ seoulog/src/test/java/com/seoulog/seoulog/SeoulogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seoulog.seoulog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeoulogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
