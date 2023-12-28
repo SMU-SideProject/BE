042487b9419aa6734a47db7b82014b7e4ff77431
xxyeon <jykim9335@gmail.com>
2023-12-28 23:01:34 +0900

    Fix: 특정 url은 jwtFilter 거치치 않게 수정
    1. 회원가입 api jwtFilter 거치치 않게 수정
    2. 로그인 api jwtFilter 거치지 않게 수정
    3. cors 설정 정보 추가

--- seoulog/src/main/java/com/seoulog/user/jwt/JwtFilter.java
+++ seoulog/src/main/java/com/seoulog/user/jwt/JwtFilter.java
@@ -2,37 +2,35 @@ package com.seoulog.user.jwt;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.StringUtils;
-import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @Slf4j
-public class JwtFilter extends GenericFilterBean {
+public class JwtFilter extends OncePerRequestFilter {
 
     private final int tokenValidityInMilliseconds = 86400;
     private static final Logger logger = LoggerFactory.getLogger(JwtFilter.class);
     public static final String AUTHORIZATION_HEADER = "Authorization";
-    private TokenProvider tokenProvider;
+    private final TokenProvider tokenProvider;
 
     public JwtFilter(TokenProvider tokenProvider) {
         this.tokenProvider = tokenProvider;
     }
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
-        String accessToken = resolveToken(httpServletRequest); //request header에서 token값 가져오기
-        String requestURI = httpServletRequest.getRequestURI();
-
+    public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        String accessToken = resolveToken(request); //request header에서 token값 가져오기
+        String requestURI = request.getRequestURI();
+        final String authorizationHeader = request.getHeader("Authorization");
         if (StringUtils.hasText(accessToken) && tokenProvider.validateToken(accessToken).equals("ACCESS")) { //토큰 검증
             Authentication authentication = tokenProvider.getAuthentication(accessToken);
             SecurityContextHolder.getContext().setAuthentication(authentication);
@@ -40,18 +38,17 @@ public class JwtFilter extends GenericFilterBean {
         } else if (StringUtils.hasText(accessToken) && tokenProvider.validateToken(accessToken).equals("Expired")) {
             log.info("Access token expired");
             String refreshToken = null;
-            if (StringUtils.hasText(httpServletRequest.getHeader("Auth"))) { // Auth에는 email 담겨 있음
-                String email = httpServletRequest.getHeader("Auth");
+            if (StringUtils.hasText(request.getHeader("Auth"))) { // Auth에는 email 담겨 있음
+                String email = request.getHeader("Auth");
                 refreshToken = tokenProvider.getRefreshToken(email); // userId로 refreshToken 조회
             }
         } else {
                 logger.debug("유효한 JWT 토큰이 없습니다, uri: {}", requestURI);
         }
 
-        filterChain.doFilter(servletRequest, servletResponse);
+        filterChain.doFilter(request, response);
         }
 
-//    }
 
     private String resolveToken(HttpServletRequest request) {
         String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
